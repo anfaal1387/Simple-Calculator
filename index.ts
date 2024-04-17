febefb3bac78e3e58ceca74ab27840d4f544c3c3
Muhammad Anfaal <161188326+anfaal1387@users.noreply.github.com>
2024-04-17 18:28:15 +0500

    Simple Calculator
    Simple Calculator project!!

---
+++ index.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
+
+//Asking questions from users through inquirer
+let answers = await inquirer.prompt([
+    {message: "Entre your first Number", type: "number", name:"firstNumber"},
+    {message: "Entre your second Number", type:"number", name:"secondNumber"},
+    {
+        message: "Select any one opertaion to perform operations!",
+        type: "list",
+        name: "Operator",
+        choices: ["Addition", "Subtraction", "Multipliction", "Division"],
+    },
+]);
+//conditional statements if else
+
+if(answers.Operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+else if(answers.Operator === "Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber)
+}
+else if(answers.Operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+else if(answers.Operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+    console.log("Invalid Input")
+}
